e44aff76f9e3d4a2e4db5369b6bc46bef719b422
Ben Westover <kwestover.kw@gmail.com>
2021-07-22 20:19:06 -0400

    commands command

--- commands/infos.py
+++ commands/infos.py
@@ -11,14 +11,25 @@ class infos(commands.Cog):
         await ctx.send(f"Pong! {round(self.bot.latency * 1000)}ms")
 
     @commands.command()
-    async def commands(self, ctx: commands.Context):
+    async def commands(self, ctx: commands.Context, arg=None):
         with open('commands.yaml', 'r') as commands_file:
             commands = yaml.load(commands_file, Loader=yaml.BaseLoader)
 
-        embed = discord.Embed(title="Command List", color=0x00a0a0)
-        for command in commands:
-            embed.add_field(name=command, value=commands[command], inline=False)
-        await ctx.send(embed=embed)
+        if not arg:
+            embed = discord.Embed(title="Command List", description="Commands come in categories. Here is a list of categories, run `!commands <category>` to see the commands in a certain category.", color=0x00a0a0)
+            embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.avatar_url)
+            for category in commands:
+                if not '_desc' in category:
+                    embed.add_field(name=category + ':', value=commands[category + '_desc'], inline=True)
+            await ctx.send(embed=embed)
+        elif arg in commands:
+            embed = discord.Embed(title="Command List", description="Commands in the {} category:".format(arg), color=0x00a0a0)
+            embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.avatar_url)
+            for command in commands[arg]:
+                embed.add_field(name=command + ':', value=commands[arg].get(command), inline=False)
+            await ctx.send(embed=embed)
+        else:
+            await ctx.send('Please send a valid category')
 
 def setup(bot):
     bot.add_cog(infos(bot))
