cdd2f85ec346952781a946a82676ce79efe38ad2
Ben Westover <kwestover.kw@gmail.com>
2021-08-02 18:20:16 -0400

    Combine two listeners

--- cogs/listeners.py
+++ cogs/listeners.py
@@ -17,48 +17,66 @@ class listeners(commands.Cog):
     global role_ids
     role_ids = config['role_ids']
 
-    @commands.Cog.listener("on_message")
-    async def swearfilter(self, message):
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        support_channels = [int(channel_ids['vc_chat'])]
+        for channel in channel_ids:
+            if channel.endswith("support"):
+                support_channels.append(int(channel_ids[channel]))
+        help_triggers = ["issue", "able to help", "get some help", "need help"]
+
         swore = ""
         for swear in swears:
             if swear in message.content:
                 swore = swear
-        if swore == "":
-           return
 
-        await message.delete()
-        if swore == "@everyone":
-           dmbed=discord.Embed(title="@everyone ping", description="Please don't ping @everyone. If you need help, go to a support channel and ping @Support Team.")
-           embed=discord.Embed(title="@everyone ping by " + str(message.author), color=0x00a0a0)
-        if swore == "@here":
-           dmbed=discord.Embed(title="@here ping", description="Please don't ping @here. If you need help, go to a support channel and ping @Support Team.")
-           embed=discord.Embed(title="@here ping by " + str(message.author), color=0x00a0a0)
-        elif swore == "discord.gg" or swore == "discord.com/invite":
-           dmbed=discord.Embed(title="Invite Link", description="Please don't send invite links to other servers, it is against rule 6 of our server.")
-           embed=discord.Embed(title="Invite Link sent by " + str(message.author), color=0x00a0a0)
-        else:
-           dmbed=discord.Embed(title="Swear", description="TEG is a PG Friendly server, you cannot swear here.")
-           dmbed.add_field(name="Swear Detected:", value=swore, inline=False)
-           embed=discord.Embed(title="Swear by " + str(message.author), color=0x00a0a0)
-           embed.add_field(name="Swear Detected:", value=swore, inline=False)
+        if swore != "":
+            await message.delete()
+            if swore == "@everyone":
+                dmbed=discord.Embed(title="@everyone ping", description="Please don't ping @everyone. If you need help, go to a support channel and ping @Support Team.")
+                embed=discord.Embed(title="@everyone ping by " + str(message.author), color=0x00a0a0)
+            if swore == "@here":
+                dmbed=discord.Embed(title="@here ping", description="Please don't ping @here. If you need help, go to a support channel and ping @Support Team.")
+                embed=discord.Embed(title="@here ping by " + str(message.author), color=0x00a0a0)
+            elif swore == "discord.gg" or swore == "discord.com/invite":
+                dmbed=discord.Embed(title="Invite Link", description="Please don't send invite links to other servers, it is against rule 6 of our server.")
+                embed=discord.Embed(title="Invite Link sent by " + str(message.author), color=0x00a0a0)
+            else:
+                dmbed=discord.Embed(title="Swear", description="TEG is a PG Friendly server, you cannot swear here.")
+                dmbed.add_field(name="Swear Detected:", value=swore, inline=False)
+                embed=discord.Embed(title="Swear by " + str(message.author), color=0x00a0a0)
+                embed.add_field(name="Swear Detected:", value=swore, inline=False)
 
-        dm = await message.author.create_dm()
-        dmbed.set_thumbnail(url=message.author.avatar_url)
-        dmbed.add_field(name="Message Deleted:", value=message.content, inline=False)
-        dm_failed = False
-        try:
-            await dm.send(embed=dmbed)
-        except:
-            dm_failed = True
+            dm = await message.author.create_dm()
+            dmbed.set_thumbnail(url=message.author.avatar_url)
+            dmbed.add_field(name="Message Deleted:", value=message.content, inline=False)
+            dm_failed = False
+            try:
+                await dm.send(embed=dmbed)
+            except:
+                dm_failed = True
 
-        embed.set_thumbnail(url=message.author.avatar_url)
-        embed.add_field(name="Message Deleted:", value=message.content, inline=False)
-        embed.add_field(name="In channel", value=message.channel.mention, inline=False)
-        embed.add_field(name="User ID", value=message.author.id, inline=False)
-        if dm_failed == True:
-            embed.set_footer(text="was not able to DM user")
-        channel = self.bot.get_channel(int(channel_ids['filter_log']))
-        await channel.send(embed=embed)
+            embed.set_thumbnail(url=message.author.avatar_url)
+            embed.add_field(name="Message Deleted:", value=message.content, inline=False)
+            embed.add_field(name="In channel", value=message.channel.mention, inline=False)
+            embed.add_field(name="User ID", value=message.author.id, inline=False)
+            if dm_failed == True:
+                embed.set_footer(text="was not able to DM user")
+            channel = self.bot.get_channel(int(channel_ids['filter_log']))
+            await channel.send(embed=embed)
+
+        elif any(trigger in message.content for trigger in help_triggers) and not message.channel.id in support_channels:
+            channel = self.bot.get_channel(message.channel.id)
+            await channel.send(f"If you're looking for help please go to a support channel like <#{channel_ids['general_support']}> and ping the <@&{role_ids['support_team']}>.", allowed_mentions=discord.AllowedMentions(roles=False))
+
+        elif "reinstall windows" in message.content:
+            channel = self.bot.get_channel(message.channel.id)
+            await channel.send("This tutorial will lead you how to do a fresh windows installation: (All your data will be gone, back it up and use `!key` in case you need to back up your Product Key too, please save it somewhere safe and don't show us or anyone the key!)\nhttps://youtu.be/bwJ_E-I9WRs\nTo figure out which key you need to use to boot to a usb, run the command `!bootkeys`.")
+
+        elif "virus" in message.content:
+            if not message.author.bot:
+                channel = self.bot.get_channel(message.channel.id)
+                await channel.send("We suggest you to check for viruses and suspicious processes with Malwarebytes: https://malwarebytes.com/mwb-download/thankyou/")
 
     @commands.Cog.listener("on_message_delete")
     async def deletelog(self, message):
@@ -95,27 +113,6 @@ class listeners(commands.Cog):
             embed.add_field(name="Edited Message:", value="Unable to detect message contents", inline=False)
         channel = self.bot.get_channel(int(channel_ids['message_edit']))
         await channel.send(embed=embed)
-
-    @commands.Cog.listener("on_message")
-    async def autoresponse(self, message):
-        support_channels = [int(channel_ids['vc_chat'])]
-        for channel in channel_ids:
-            if channel.endswith("support"):
-                support_channels.append(int(channel_ids[channel]))
-
-        help_triggers = ["issue", "able to help", "get some help", "need help"]
-        if any(trigger in message.content for trigger in help_triggers) and not message.channel.id in support_channels:
-            channel = self.bot.get_channel(message.channel.id)
-            await channel.send(f"If you're looking for help please go to a support channel like <#{channel_ids['general_support']}> and ping the <@&{role_ids['support_team']}>.", allowed_mentions=discord.AllowedMentions(roles=False))
-
-        if "reinstall windows" in message.content:
-            channel = self.bot.get_channel(message.channel.id)
-            await channel.send("This tutorial will lead you how to do a fresh windows installation: (All your data will be gone, back it up and use `!key` in case you need to back up your Product Key too, please save it somewhere safe and don't show us or anyone the key!)\nhttps://youtu.be/bwJ_E-I9WRs\nTo figure out which key you need to use to boot to a usb, run the command `!bootkeys`.")
-
-        if "virus" in message.content:
-            if not message.author.bot:
-                channel = self.bot.get_channel(message.channel.id)
-                await channel.send("We suggest you to check for viruses and suspicious processes with Malwarebytes: https://malwarebytes.com/mwb-download/thankyou/")
 
 def setup(bot):
     bot.add_cog(listeners(bot))
