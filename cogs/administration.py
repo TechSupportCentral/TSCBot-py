dde023e49a69071f65a57e9d76cd41ae4f075d8e
Ben Westover <kwestover.kw@gmail.com>
2021-08-12 23:01:31 -0400

    Suggestions

--- cogs/administration.py
+++ cogs/administration.py
@@ -77,15 +77,12 @@ class administration(commands.Cog):
         else:
             dm = member.dm_channel
         dmbed = discord.Embed(title="Message from the owner of TSC", description=' '.join(args), color=0x00a0a0)
-        dm_failed = False
         try:
             await dm.send(embed=dmbed)
         except:
-            dm_failed = True
-
-        if dm_failed == True:
             await ctx.send("Failed to DM the user")
             return
+
         await ctx.message.add_reaction("✅")
         embed = discord.Embed(title="DM Sent", color=discord.Color.green())
         embed.set_thumbnail(url=member.avatar_url)
@@ -94,6 +91,98 @@ class administration(commands.Cog):
         embed.add_field(name="Message:", value=' '.join(args), inline=False)
         channel = self.bot.get_channel(int(channel_ids['bot_dm']))
         await channel.send(embed=embed)
+
+    @commands.command(name="accept-suggestion")
+    async def accept_suggestion(self, ctx, id=None, *args):
+        guild = ctx.message.guild
+        owner_role = guild.get_role(int(role_ids['owner']))
+        if not owner_role in ctx.message.author.roles:
+            await ctx.send("You do not have permission to run this command.")
+            return
+
+        if not id:
+            await ctx.send("Feed me some arguments please")
+            return
+        elif not id.isdigit():
+            await ctx.send("Please mention what suggestion you'd like to accept via the message ID.")
+            return
+        channel = self.bot.get_channel(int(channel_ids['suggestions_list']))
+        try:
+            await channel.fetch_message(id)
+        except:
+            await ctx.send("Not a valid message ID.")
+            return
+
+        if not args:
+            args = ['No', 'reason', 'provided.']
+
+        message = await channel.fetch_message(id)
+        embed = discord.Embed(description=message.embeds[0].description, color=discord.Color.green())
+        embed.set_author(name=message.embeds[0].author.name, icon_url=message.embeds[0].author.icon_url)
+        embed.add_field(name="Status: Accepted", value=' '.join(args))
+        await message.edit(embed=embed)
+
+        dmbed = discord.Embed(title="Your suggestion was accepted by the owner.", description=message.embeds[0].description, color=discord.Color.green())
+        dmbed.add_field(name="Reason:", value=' '.join(args))
+
+        member = guild.get_member_named(message.embeds[0].author.name)
+        if member.dm_channel is None:
+            dm = await member.create_dm()
+        else:
+            dm = member.dm_channel
+        try:
+            await dm.send(embed=dmbed)
+        except:
+            await ctx.send("The suggestion was accepted successfully but the the user was unable to be DMed.")
+            return
+
+        await ctx.message.add_reaction("✅")
+
+    @commands.command(name="decline-suggestion")
+    async def decline_suggestion(self, ctx, id=None, *args):
+        guild = ctx.message.guild
+        owner_role = guild.get_role(int(role_ids['owner']))
+        if not owner_role in ctx.message.author.roles:
+            await ctx.send("You do not have permission to run this command.")
+            return
+
+        if not id:
+            await ctx.send("Feed me some arguments please")
+            return
+        elif not id.isdigit():
+            await ctx.send("Please mention what suggestion you'd like to decline via the message ID.")
+            return
+        channel = self.bot.get_channel(int(channel_ids['suggestions_list']))
+        try:
+            await channel.fetch_message(id)
+        except:
+            await ctx.send("Not a valid message ID.")
+            return
+
+        if not args:
+            args = ['No', 'reason', 'provided.']
+
+        message = await channel.fetch_message(id)
+        embed = discord.Embed(description=message.embeds[0].description, color=discord.Color.red())
+        embed.set_author(name=message.embeds[0].author.name, icon_url=message.embeds[0].author.icon_url)
+        embed.add_field(name="Status: Declined", value=' '.join(args))
+        await message.edit(embed=embed)
+
+        dmbed = discord.Embed(title="Your suggestion was declined by the owner.", description=message.embeds[0].description, color=discord.Color.red())
+        dmbed.add_field(name="Reason:", value=' '.join(args))
+
+        member = guild.get_member_named(message.embeds[0].author.name)
+        if member.dm_channel is None:
+            dm = await member.create_dm()
+        else:
+            dm = member.dm_channel
+        try:
+            await dm.send(embed=dmbed)
+        except:
+            await ctx.send("The suggestion was declined successfully but the the user was unable to be DMed.")
+            return
+
+        await ctx.message.add_reaction("✅")
 
 def setup(bot):
     bot.add_cog(administration(bot))
--- cogs/misc.py
+++ cogs/misc.py
@@ -2,17 +2,19 @@ from discord.ext import commands
 import discord
 import yaml
 
-class infos(commands.Cog):
+class misc(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    with open('config.yaml', 'r') as config_file:
+        config = yaml.load(config_file, Loader=yaml.BaseLoader)
+    global channel_ids
+    channel_ids = config['channel_ids']
+    global role_ids
+    role_ids = config['role_ids']
+
     @commands.command()
     async def alert(self, ctx, *args):
-        with open('config.yaml', 'r') as config_file:
-            config = yaml.load(config_file, Loader=yaml.BaseLoader)
-        channel_ids = config['channel_ids']
-        role_ids = config['role_ids']
-
         if args:
             alert = ' '.join(args)
         else:
@@ -23,6 +25,18 @@ class infos(commands.Cog):
         channel = self.bot.get_channel(int(channel_ids['modtalk']))
         await channel.send(f"<@&{role_ids['moderator']}> <@&{role_ids['trial_mod']}>", embed=embed)
         await ctx.send("The moderators have been alerted.")
+
+    @commands.command()
+    async def suggest(self, ctx, *args):
+        if not args:
+            await ctx.send("Please provide a suggestion.")
+            return
+        embed = discord.Embed(description=f"**Suggestion:** {' '.join(args)}", color=discord.Color.lighter_grey())
+        embed.set_author(name=ctx.author, icon_url=ctx.author.avatar_url)
+        embed.add_field(name="Status", value="Pending")
+        channel = self.bot.get_channel(int(channel_ids['suggestions_list']))
+        await channel.send(embed=embed)
+        await ctx.message.add_reaction("✅")
 
 def setup(bot):
     bot.add_cog(misc(bot))
