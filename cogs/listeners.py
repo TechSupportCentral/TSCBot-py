ea80675905daa46a17cb79de73a73070060b4fcb
Philipp's Pentium <zimbob69@yahoo.com>
2021-08-31 01:49:20 +0200

    pong

--- cogs/listeners.py
+++ cogs/listeners.py
@@ -125,15 +125,15 @@ class listeners(commands.Cog):
         elif custom != "":
             await message.channel.send(custom)
 
-        elif any(trigger in message.content.lower for trigger in help_triggers) and not message.channel.id in support_channels and message.channel.id in public_channels:
+        elif any(trigger in message.content.lower() for trigger in help_triggers) and not message.channel.id in support_channels and message.channel.id in public_channels:
             channel = self.bot.get_channel(message.channel.id)
             await channel.send(f"If you're looking for help please go to a support channel like <#{channel_ids['general_support']}> and ping the <@&{role_ids['support_team']}>.", allowed_mentions=discord.AllowedMentions(roles=False))
 
-        elif "reinstall windows" in message.content.lower:
+        elif "reinstall windows" in message.content.lower():
             channel = self.bot.get_channel(message.channel.id)
             await channel.send("This tutorial will lead you how to do a fresh windows installation: (All your data will be gone, back it up and use `!key` in case you need to back up your Product Key too, please save it somewhere safe and don't show us or anyone the key!)\nhttps://youtu.be/bwJ_E-I9WRs\nTo figure out which key you need to use to boot to a usb, run the command `!bootkeys`.")
 
-        elif " virus" in message.content.lower:
+        elif " virus" in message.content.lower():
             if not message.author.bot:
                 channel = self.bot.get_channel(message.channel.id)
                 await channel.send("We suggest you to check for viruses and suspicious processes with Malwarebytes: https://malwarebytes.com/mwb-download/thankyou/")
@@ -179,6 +179,8 @@ class listeners(commands.Cog):
             await message.delete()
             reactcol = mongodb['reaction-roles']
             reacts = reactcol.find()
+        elif "pong" in message.content.lower() and message.author.id == 655487743694209063:
+            await message.delete()
 
     @commands.Cog.listener()
     async def on_message_delete(self, message):
@@ -297,7 +299,6 @@ class listeners(commands.Cog):
             if invite.uses > findinvite(invites, invite.code).uses:
                 invite_used = invite
                 invites = afterinvites
-                return
 
         embed2 = discord.Embed(title="Member Joined", description=member, color=discord.Color.green())
         embed2.set_thumbnail(url=member.avatar_url)
