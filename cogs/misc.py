7eb4166b0caeccf3ac7735b4db7bf6cbc102ce8d
Ben Westover <kwestover.kw@gmail.com>
2021-11-02 20:34:27 -0400

    First discord command that can be used from matrix

--- cogs/misc.py
+++ cogs/misc.py
@@ -42,5 +42,53 @@ class misc(commands.Cog):
     async def d(self, ctx):
         return
 
+    @commands.Cog.listener(name="on_message")
+    async def matrix_moderation(self, message):
+
+        async def userinfo(self, message):
+            guild = message.guild
+
+            if len(message.content.split(" ")) >= 2:
+                arg = message.content.split(" ")[1]
+            else:
+                await message.channel.send("You can't do a userinfo on yourself, since you're a Matrix user.")
+                return
+
+            if "<@" in arg:
+                id = arg
+                id = id.replace("<", "")
+                id = id.replace(">", "")
+                id = id.replace("@", "")
+                id = id.replace("!", "")
+                id = int(id)
+            elif arg.isdigit():
+                id = int(arg)
+            else:
+                await message.channel.send("Users have to be in the form of an ID or a mention.")
+                return
+
+            if guild.get_member(id) is None:
+                await ctx.send("User is not in the server.")
+                return
+
+            member = guild.get_member(id)
+            embed=discord.Embed(title=member, color=0x00a0a0)
+            embed.set_thumbnail(url=member.avatar_url)
+            embed.add_field(name="User ID", value=id, inline=False)
+            if member.name != member.display_name:
+                embed.add_field(name="Nickname", value=member.display_name, inline=False)
+            embed.add_field(name="Account Created:", value=member.created_at.strftime("%-d %B %Y at %-H:%M"), inline=True)
+            embed.add_field(name="Joined Server:", value=member.joined_at.strftime("%-d %B %Y at %-H:%M"), inline=True)
+            mention = []
+            for role in member.roles:
+                if role.name != "@everyone":
+                        mention.append(role.mention)
+            roles = ", ".join(mention)
+            embed.add_field(name="Roles", value=roles, inline=False)
+            await message.channel.send(embed=embed)
+
+        if message.content.startswith("d!userinfo"):
+            await userinfo(self, message)
+
 def setup(bot):
     bot.add_cog(misc(bot))
